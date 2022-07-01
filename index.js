5056210b6b35b1133b72251ea042bcdb5388dee9
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-07-02 00:21:35 +0600

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -22,9 +22,36 @@ async function run(){
         const dbcollections = client.db('todoTask').collection('tasks');
 
         app.post('/task',async(req,res)=>{
-            const addproduct = req.body;
-            const result = await dbcollections.insertOne(addproduct);
+            const task = req.body;
+            const result = await dbcollections.insertOne(task);
             res.send(result)
+        })
+
+        app.get('/task',async(req,res)=>{
+            const query = {}
+            const cursor =  dbcollections.find(query);
+            const task = await cursor.toArray();
+            res.send(task);
+        })
+
+        app.delete('/task/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result =await dbcollections.deleteOne(query);
+            res.send(result); 
+        })
+
+        app.put('/task/:id',async(req,res)=>{
+            const id = req.params.id;
+            const task = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: task
+                    
+            };
+            const result =await dbcollections.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
 
     }
